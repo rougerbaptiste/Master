e39868fe8e488d4a0e48b1889969845fa9e64782
Baptiste Rouger <rouger.b@gmail.com>
2016-09-07 10:29:57 +0200

    Updating Matlab first session

--- Matlab/example.m
+++ Matlab/example.m
@@ -7,4 +7,6 @@ end
 c = 4 ;
 f = d-c;
 
-% we can use clear to delete all or some varaibles.
+% we can use clear to delete all or some variables.
+% we can use load(pack_of_variables.mat, var) to only load var.
+% to get acces to the documentation of a function, we use doc ...
---
+++ Matlab/exercice1.m
@@ -0,0 +1,1 @@
+round(b * chiara + a)
