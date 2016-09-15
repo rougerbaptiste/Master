e9d498e91f0c07b70b2ac23ec573b6041dcd4cf8
Baptiste Rouger <rouger.b@gmail.com>
2016-09-15 13:02:39 +0200

    Updating Matlab prey-pred model

--- Bootcamp/Matlab/Session7/prey_pred_mod.m
+++ Bootcamp/Matlab/Session7/prey_pred_mod.m
@@ -7,34 +7,54 @@
 %%%%%%%%%%
 % Here we can define the parameters
 
-prey = 10 000; % Initial number of preys
-pred = 10; % Initial number of predators
-lambdaPrey = (1/(2*2))*8; % We assume that 2 rabbits have 8 rabbits every 2 mounths
+prey = 200; % Initial number of preys
+pred = 2; % Initial number of predators
+lambdaPrey = (1/(2*2))*6; % We assume that 2 rabbits have 8 rabbits every 2 mounths
 lambdaPred = (1/(2*6))*4; %It takes 6 mounth to the wolves to produce 4 puppy
-n = 100;
-deltaPred = 1/120;
-deltaPrey = 1/60;
-TSpan = 100; % The length of the simulation in mounths.
+n = 200; % number of rabbits needed to produce the puppies
+deltaPred = 1/240; % the rate at which the predators die (so each 240 mounths)
+deltaPrey = 1/60; % the rate at which the preys die (so each 60 mounths)
+TSpan = 3; % The length of the simulation in mounths.
+
 
 %%%%%%%%%%
 % Here we define some useful variables for the simulation
 dPrey = 0;
 dPred = 0;
-preys = zeros(TSpan);
-preds = zeros(TSpam);
+preys = zeros(TSpan+1,1);
+preds = zeros(TSpan+1,1);
 
 preys(1) = prey;
 preds(1) = pred;
 
-for i=1:100
-	dPrey = prey + prey*lambdaPrey;
-	dPrey = dPrey - prey*lambdaPred*n;
-	dPred = pred + pred*lambdaPred;
-	dPred = dPred - pred*deltaPred;
-	
+%%%%%%%%%
+% Now we enter the simulation loop
 
+for i=1:TSpan
+	dPrey = prey + prey*lambdaPrey; % compute the born preys
+	dPrey = dPrey - pred*lambdaPred*n; % compute the eaten preys
+
+	dPred = pred + pred*lambdaPred; % compute the born predators
+
+	dPred = dPred - pred*deltaPred; % compute the dead predators
+	dPrey = dPrey - prey*deltaPrey; % compute the dead preys
+
+	%%%%%
+	% Here we store our variable for the next iteration of the loop and the plot
 	prey = dPrey;
 	pred = dPred;
 	preys(i+1) = prey;
 	preds(i+1) = pred;
 end
+
+absc = 0:TSpan; % a vector that contains the mounths of the simulation
+
+%%%%%%%%%%
+% Now we plot the result
+subplot(2,1,1)
+plot(absc,preys,'r--');
+hold on;
+subplot(2,1,2)
+plot(absc,preds,'b--');
+
+pause;
